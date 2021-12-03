dbbc2a249b54659b67a1720bdc46612ee248336f
MarioSalazarD <20153314@aloe.ulima.edu.pe>
2021-12-03 13:48:13 -0500

    Update 20211203182923-partida.js

--- seeders/20211203182923-partida.js
+++ seeders/20211203182923-partida.js
@@ -11,6 +11,36 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+     await queryInterface.bulkInsert('Partidas',[
+      {
+        id:1,
+        fecha: "3-12-2021",
+        hora:"18:30",
+        duracion: "90",
+        equipoA:"TeamAF",
+        equipoB:"TeamBF",
+        factorA: 1.1,
+        factorB: 1.3,
+        selector: "TeamBF",
+        juegoId:1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id:2,
+        fecha: "5-12-2021",
+        hora:"15:00",
+        duracion: "90",
+        equipoA:"TeamAV",
+        equipoB:"TeamBV",
+        factorA: 1.2,
+        factorB: 1.6,
+        selector: "TeamBV",
+        juegoId:2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ],{})
   },
 
   down: async (queryInterface, Sequelize) => {
